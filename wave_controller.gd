3053240514ba760b792826c99ec951d7b4fda83d
samuelim01 <samuelim01@gmail.com>
2023-05-20 00:29:27 +0800

    Removed wave controller constants
    Modified wave controller to read from .json file instead

--- wave_controller.gd
+++ wave_controller.gd
@@ -4,24 +4,17 @@ extends Node2D
 @export var mob_scene: PackedScene
 @export var mob_spawn_location: PathFollow2D
 
-const MOB_NUMBER := [5, 10, 20, 40, 8]
-const MOB_SPEED := [75.0, 100.0, 200.0, 50.0, 250.0]
-const MOB_HEALTH := [10.0, 10.0, 5.0, 50.0, 5.0]
-const MOB_DAMAGE := [5.0, 10.0, 5.0, 20.0, 10.0]
-const COOLDOWN := [10.0, 10.0, 10.0, 15.0, 15.0]
-
 var cur_wave := 0
+var wave_config: Array
 
 func _ready() -> void:
 	randomize()
-
-
-func _process(delta: float) -> void:
-	pass
+	var file_name = "res://config/wave_config.json"
+	var file = FileAccess.open(file_name, FileAccess.READ)
+	wave_config = JSON.parse_string(file.get_as_text())
 
 
 func start() -> void:
-	# Restart
 	cur_wave = 0
 	start_wave()
 
@@ -33,17 +26,19 @@ func start_wave() -> void:
 	# Ensuring no errors
 	if not mob_spawn_location:
 		return
-	if cur_wave >= MOB_NUMBER.size():
+	if cur_wave >= wave_config.size():
 		return
 	
-	for i in range(MOB_NUMBER[cur_wave]):
+	var cur_wave_config = wave_config[cur_wave]
+	
+	for i in range(cur_wave_config["count"]):
 		mob_spawn_location.progress_ratio = randf()
 		var mob = mob_scene.instantiate()
 		mob.position = mob_spawn_location.position
-		mob.speed = MOB_SPEED[cur_wave]
-		mob.set_max_health(MOB_HEALTH[cur_wave])
-		mob.set_damage(MOB_DAMAGE[cur_wave])
+		mob.set_max_health(cur_wave_config["health"])
+		mob.speed = cur_wave_config["speed"]
+		mob.set_damage(cur_wave_config["damage"])
 		add_child(mob)
 	
-	$WaveTimer.start(COOLDOWN[cur_wave])
+	$WaveTimer.start(cur_wave_config["cooldown"])
 	cur_wave += 1
