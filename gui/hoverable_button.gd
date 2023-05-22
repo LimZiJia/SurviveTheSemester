51128716f23aa0904f28972def0c053aabf669e2
samuelim01 <samuelim01@gmail.com>
2023-05-23 00:35:27 +0800

    Added start menu
    1) Added start menu and made it main scene
    2) Added custom button that expands upon hover
    3) Reinitialised player animation upon game start so that player is not facing upside down
    4) Refixed ghost melee upon game start

---
+++ gui/hoverable_button.gd
@@ -0,0 +1,15 @@
+class_name HoverableButton
+extends Button
+
+var original_size := scale
+var grow_size := Vector2(1.1, 1.1)
+var dur := 0.1
+
+func _ready() -> void:
+	mouse_entered.connect(resize_button.bind(grow_size))
+	mouse_exited.connect(resize_button.bind(original_size))
+
+
+func resize_button(size: Vector2):
+	var tween = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self, "scale", size, dur)
--- gui/hud.gd
+++ gui/hud.gd
@@ -2,36 +2,50 @@ extends CanvasLayer
 
 signal start_game
 
+@onready var score_label = $ScoreLabel
+@onready var message_label = $MessageLabel
+@onready var buttons = $Buttons
+@onready var play_button = $Buttons/PlayButton
+@onready var main_menu_button = $Buttons/MainMenuButton
+@onready var message_timer = $MessageTimer
+
+func _ready() -> void:
+	play_button.pressed.connect(play_again)
+	main_menu_button.pressed.connect(go_to_main)
+
 func update_score(score: int) -> void:
-	$ScoreLabel.text = str(score)
+	score_label.text = str(score)
 
 
 func show_message(text: String) -> void:
-	$MessageLabel.text = text
-	$MessageLabel.show()
+	message_label.text = text
+	message_label.show()
 
 
 func hide_message() -> void:
-	$MessageLabel.hide()
+	message_label.hide()
 
 
 func show_message_with_time(text: String, time: float) -> void:
 	show_message(text)
-	$MessageTimer.start(time)
+	message_timer.start(time)
 
 
 func show_game_over() -> void:
 	show_message_with_time("Game Over", 2.0)
-	await($MessageTimer.timeout)
+	await(message_timer.timeout)
 	show_message("Survive The Semester")
 	await(get_tree().create_timer(1.0).timeout)
-	$StartButton.show()
+	buttons.show()
 
 
-func _on_start_button_pressed():
-	$StartButton.hide()
+func play_again() -> void:
+	buttons.hide()
 	emit_signal("start_game")
 
 
+func go_to_main() -> void:
+	get_tree().change_scene_to_file("res://gui/start_screen.tscn")
+
 func _on_message_timer_timeout():
-	$MessageLabel.hide()
+	message_label.hide()
