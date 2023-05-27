f34b7ca412eccfdafb8f664f2b408c76b97e007f
samuelim01 <samuelim01@gmail.com>
2023-05-27 10:43:17 +0800

    Improved GUI
    1) Replaced empty VBoxContainer to make use of container's separation field
    2) Documented settings script

--- gui/settings_menu.gd
+++ gui/settings_menu.gd
@@ -1,71 +1,77 @@
 extends Control
 
 @onready var grid = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/GridContainer
-@onready var reset_button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/ResetButton
-@onready var back_button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/BackButton
+@onready var reset_button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer/ResetButton
+@onready var back_button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer/BackButton
 
-var target: Button = null
-var actions = [
+var button_pressed: Button = null
+var actions := [
 	"move_up", "move_left", "move_down", "move_right", "attack"
 ]
-var events := {}
+var button_action_dict: Dictionary
 
 func _ready() -> void:
 	back_button.pressed.connect(go_to_main)
 	reset_button.pressed.connect(reset)
 	
-	for action in actions:
+	# Add all actions into the scene grid
+	for action in actions: 
 		var label = Label.new()
-		label.text = " ".join(action.split("_")).capitalize()
+		label.add_theme_font_size_override("font_size", 32)
 		label.set("size_flags_horizontal", 3)
+		label.text = " ".join(action.split("_")).capitalize()
 		grid.add_child(label)
 		
 		var button = Button.new()
-		var event := InputMap.action_get_events(action)[0]
-		label.add_theme_font_size_override("font_size", 32)
-		button.text = OS.get_keycode_string(event.physical_keycode)
 		button.add_theme_font_size_override("font_size", 32)
 		button.set("custom_minimum_size", Vector2(210.0, 0.0))
+		button.text = get_key(action)
 		grid.add_child(button)
 		
 		button.pressed.connect(_on_button_pressed.bind(button))
-		events[button.to_string()] = action
+		button_action_dict[button.to_string()] = action
 
 
 func go_to_main() -> void:
 	SceneChanger.change_scene("res://gui/start_screen.tscn")
 
-
+# Resets all actions to their original input keys
 func reset() -> void:
 	InputMap.load_from_project_settings()
 	SceneChanger.reload_scene()
 
 func _on_button_pressed(button: Button) -> void:
 	button.text = "Press key ..."
-	target = button
+	button_pressed = button
 
+# Remaps the input key of an action if an action is selected. If the
+# selected input key is already assigned to another action, it does not
+# remap and displays its original input key.
 func _input(event: InputEvent) -> void:
-	if not event is InputEventKey or not event.pressed or \
-		target == null:
+	if (button_pressed == null or not event is InputEventKey
+		or not event.pressed):
 		return
 	
-	var action = events[target.to_string()]
+	var action = button_action_dict[button_pressed.to_string()]
 	
-	if key_already_used(event):
-		var old_event := InputMap.action_get_events(action)[0]
-		target.text = OS.get_keycode_string(old_event.physical_keycode)
+	if key_is_active(event):
+		button_pressed.text = get_key(action)
 		return
-	
-	target.text = OS.get_keycode_string(event.key_label)
-	target = null
 	
 	InputMap.action_erase_events(action)
 	InputMap.action_add_event(action, event)
+	button_pressed.text = get_key(action)
+	button_pressed = null
 
-
-func key_already_used(event: InputEvent) -> bool:
+# Returns true if the current InputEventKey is already assigned to another action
+func key_is_active(event: InputEventKey) -> bool:
 	for action in actions:
 		var action_event = InputMap.action_get_events(action)[0]
 		if event.key_label == action_event.physical_keycode:
 			return true
 	return false
+
+# Returns the string representation of the input key mapped to the action
+func get_key(action: StringName) -> StringName:
+	var event: InputEventKey = InputMap.action_get_events(action)[0]
+	return OS.get_keycode_string(event.physical_keycode)
