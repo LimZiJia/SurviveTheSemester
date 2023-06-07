103149c9ddeccf0ae16b6a16997fb73a4a3e6bc8
samuelim01 <samuelim01@gmail.com>
2023-06-07 10:58:22 +0800

    Added more grades
    1) Added more grades
    2) Combined grade png files into a single spritesheet

--- overlays/statuses.gd
+++ overlays/statuses.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
-var grade_arr:Array = [
-	preload("res://assets/A+.png"),
-	preload("res://assets/A.png"),
-	preload("res://assets/B.png"),
-	preload("res://assets/C.png"),
-	preload("res://assets/D.png"),
-	preload("res://assets/F.png")
+const GRADE_BOUNDARIES := [
+	95, 90, 85, 50, 75, 60, 50, 40, 30, 20, 0
 ]
 
-@onready var health_bar = $HealthBar
-@onready var health_grade = $HealthBar/Grade
+@onready var health_bar := $HealthBar as TextureProgressBar
+@onready var health_grade := $HealthBar/Grade as Sprite2D
 
 func update_health(frac_cur_health: float) -> void:
 	health_bar.value = int(frac_cur_health * 100)
@@ -18,15 +13,7 @@ func update_health(frac_cur_health: float) -> void:
 
 
 func update_grades(percent: float) -> void:
-	if (percent >= 90):
-		health_grade.texture = grade_arr[0]
-	elif (percent >= 80):
-		health_grade.texture = grade_arr[1]
-	elif (percent >= 70):
-		health_grade.texture = grade_arr[2]
-	elif (percent >= 60):
-		health_grade.texture = grade_arr[3]
-	elif (percent >= 50):
-		health_grade.texture = grade_arr[4]
-	else:
-		health_grade.texture = grade_arr[5]
+	for i in GRADE_BOUNDARIES.size():
+		if percent >= GRADE_BOUNDARIES[i]:
+			health_grade.frame = i
+			break
