7e5ce969258e9877553635f85f08c22a9e3d1621
samuelim01 <samuelim01@gmail.com>
2023-06-09 22:54:45 +0800

    Fixed experience bar having internal script

--- scenes/ui/experience_bar.gd
+++ scenes/ui/experience_bar.gd
@@ -1,1 +1,18 @@
 extends CanvasLayer
+
+@export var experience_manager: Node
+
+@onready var progress_bar := $TextureProgressBar as TextureProgressBar
+
+func _ready() -> void:
+	progress_bar.value = 0
+	
+	if experience_manager == null:
+		return
+	
+	experience_manager.experience_updated.connect(on_experience_updated)
+
+
+func on_experience_updated(current_experience: int, target_experience: int):
+	progress_bar.value = current_experience
+	progress_bar.max_value = target_experience
