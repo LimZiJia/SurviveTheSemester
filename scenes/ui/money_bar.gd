255a3993b969f55df3cd251a721edb6404f3c416
samuelim01 <samuelim01@gmail.com>
2023-06-10 02:07:59 +0800

    Added animations for money bar
    Added tween animation for when money is updated. While the money label does expand when necessary, the animation coupled with the differing sizes between numbers result in a whirring motion where the money label keeps changing in size.

---
+++ scenes/ui/money_bar.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+const TWEEN_TIME := 0.5
+
+var actual_value: int = 0
+var display_value: int = 0:
+	set(value):
+		display_value = value
+		money_label.text = "%05d" % display_value
+
+@onready var money_label := $MoneyLabel as Label
+
+
+func update_value(new_value: int) -> void:
+	actual_value = new_value
+	
+	var tween = get_tree().create_tween()
+	
+	tween.tween_property(self, "display_value", actual_value, TWEEN_TIME)
