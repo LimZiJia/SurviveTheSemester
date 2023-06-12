4745bab90740a414d72d537c15bb727f832d3460
samuelim01 <samuelim01@gmail.com>
2023-06-12 14:28:52 +0800

    Improved money bar tween
    Kills any previous tweens when animating now

--- scenes/ui/money_bar.gd
+++ scenes/ui/money_bar.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 const TWEEN_TIME := 0.5
 
+var tween: Tween
 var actual_value: int = 0
 var display_value: int = 0:
 	set(value):
 		display_value = value
 		money_label.text = "%05d" % display_value
+
 
 @onready var money_label := $MoneyLabel as Label
 
@@ -14,6 +16,8 @@ var display_value: int = 0:
 func update_value(new_value: int) -> void:
 	actual_value = new_value
 	
-	var tween = get_tree().create_tween()
+	if tween:
+		tween.kill()
+	tween = create_tween()
 	
 	tween.tween_property(self, "display_value", actual_value, TWEEN_TIME)
