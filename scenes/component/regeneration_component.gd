0361e94312e6a57f3871af29c9035ecd994572f2
samuelim01 <samuelim01@gmail.com>
2023-06-12 14:38:36 +0800

    Added regeneration component
    Note that the component cannot be obtained yet

---
+++ scenes/component/regeneration_component.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var health_component: HealthComponent
+@export var health_per_second := 1.0
+
+var cooldown := 5.0
+
+@onready var timer := $Timer as Timer
+
+
+func _ready() -> void:
+	timer.wait_time = cooldown
+	timer.timeout.connect(on_timer_timeout)
+	
+	timer.start()
+
+
+func on_timer_timeout() -> void:
+	if health_component:
+		health_component.heal(health_per_second * cooldown)
