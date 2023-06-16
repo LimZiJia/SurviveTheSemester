5eeeed62f064d9aebd08c8b7ca794bb59d161610
samuelim01 <samuelim01@gmail.com>
2023-06-16 13:51:33 +0800

    Added weapon_upgrades
    1) Added weapon_upgrade and weapon_upgrades classes. weapon_upgrade encapsulates a singular weapon upgrade. weapon_upgrades encapsulates all weapon upgrades for a particular weapon.
    2) Added basic calculator weapon upgrades

---
+++ resources/weapon_upgrades/weapon_upgrade.gd
@@ -0,0 +1,8 @@
+class_name WeaponUpgrade
+extends Resource
+
+@export var id: String
+@export_multiline var description: String
+@export var level: int
+@export var cost: int
+@export var weapon_stat: WeaponStat
---
+++ resources/weapon_upgrades/weapon_upgrades.gd
@@ -0,0 +1,11 @@
+class_name WeaponUpgrades
+extends Resource
+
+# The name of the given weapon the upgrades are being defined for
+@export var weapon_name: String
+
+# The weapon_upgrades available.
+@export var weapon_upgrades: Array[WeaponUpgrade]
+
+# The weapon controller scene to instance when a weapon is newly addded.
+@export var weapon_scene: PackedScene
