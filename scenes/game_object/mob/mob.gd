33eb985c893d32b7576f2f109922591dea7ff814
samuelim01 <samuelim01@gmail.com>
2023-06-26 01:01:16 +0800

    Performed some balancing
    1) Added tentative wave cost function
    2) Added set difficulty logic for enemies
    3) Different enemies now are gradually added to the pool of potential enemies
    4) Performed some balancing on all possible stats

--- scenes/game_object/mob/mob.gd
+++ scenes/game_object/mob/mob.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+var base_health: float
+var base_speed: float
+var base_damage: float
+
 @onready var health_component := $HealthComponent as HealthComponent
 @onready var velocity_component := $VelocityComponent as VelocityComponent
 @onready var hitbox_component := $HitboxComponent as HitboxComponent
@@ -7,6 +11,10 @@ extends CharacterBody2D
 
 
 func _ready() -> void:
+	base_health = health_component.max_health
+	base_speed = velocity_component.max_speed
+	base_damage = hitbox_component.damage
+	
 	health_component.damaged.connect(on_health_component_damaged)
 	update_health_bar()
 
@@ -22,6 +30,19 @@ func on_health_component_damaged(_damage: float) -> void:
 
 func update_health_bar() -> void:
 	health_bar.value = health_component.current_health / health_component.max_health
+
+
+# Set enemy stats based on difficulty provided
+func set_difficulty(difficulty: int) -> void:
+	if not is_inside_tree():
+		await ready
+	
+	health_component.max_health = base_health * pow(difficulty, 0.1)
+	health_component.current_health = base_health * pow(difficulty, 0.1)
+	velocity_component.max_speed = base_speed * pow(difficulty, 0.07)
+	hitbox_component.damage = base_damage * pow(difficulty, 0.15)
+	
+	update_health_bar()
 
 
 # Temporary function to set the stats of each enemy
--- test/wave_manager.gd
+++ test/wave_manager.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+const MIN_SPAWN_RADIUS := 720.0
+const MAX_SPAWN_RADIUS := 1080.0
+
 # Each dictionary has the following structure:
 # {
 #   "scene": PackedScene,
@@ -8,9 +11,6 @@ extends Node2D
 # }
 @export var enemies: Array[Dictionary] = []
 
-@export var min_wave_time := 5.0
-@export var max_wave_time := 20.0
-
 var cur_wave: int = 1
 var enemy_table := WeightedTable.new()
 
@@ -18,20 +18,42 @@ var enemy_table := WeightedTable.new()
 
 func _ready() -> void:
 	# TODO: Start with only certain enemies
-	for enemy in enemies:
-		enemy_table.add_item(enemy, enemy.weight)
+	enemy_table.add_item(
+		{
+			"cost": 2.0,
+			"scene": preload("res://scenes/game_object/mob/mob.tscn"),
+			"weight": 60,
+		},
+		60
+	)
+	
 	$Timer.timeout.connect(on_timer_timeout)
 	start_wave()
 
 
 func on_timer_timeout() -> void:
 	# TODO: Add certain mobs at certain times/waves
+	match cur_wave:
+		5: enemy_table.add_item(
+			{
+				"cost": 2.4,
+				"scene": preload("res://scenes/game_object/book/book.tscn"),
+				"weight": 40,
+			},
+			40)
+		10: enemy_table.add_item(
+			{
+				"cost": 30,
+				"scene": preload("res://scenes/game_object/bookshelf/bookshelf.tscn"),
+				"weight": 2,
+			},
+			2)
 	start_wave()
 
 
-func get_cur_wave_cost(wave_time: float) -> float:
+func get_target_cost() -> float:
 	# TODO: Create mathematical formula for cost
-	return cur_wave * wave_time
+	return 6 * log(cur_wave) + 10
 
 
 func get_spawn_position() -> Vector2:
@@ -41,7 +63,8 @@ func get_spawn_position() -> Vector2:
 		return Vector2.ZERO
 	
 	while true:
-		var spawn_position := player.global_position + Vector2.from_angle(randf_range(0, TAU)) * 720.0
+		var spawn_radius := randf_range(MIN_SPAWN_RADIUS, MAX_SPAWN_RADIUS)
+		var spawn_position := player.global_position + Vector2.from_angle(randf_range(0, TAU)) * spawn_radius
 		if absf(spawn_position.x) < 1160.0 and absf(spawn_position.y) < 680.0:
 			return spawn_position
 	
@@ -51,23 +74,26 @@ func get_spawn_position() -> Vector2:
 func start_wave() -> void:
 	cur_wave += 1
 	
-	var wave_time := randf_range(min_wave_time, max_wave_time)
-	$Timer.wait_time = wave_time
-	
-	var total_cost = get_cur_wave_cost(wave_time)
+	var target_cost = get_target_cost()
+	var actual_cost: int = 0
 	
 	var entities = get_tree().get_first_node_in_group("entities_layer") as Node2D
 	if entities == null:
 		return
 	
-	while total_cost > 0:
+	while target_cost > 0:
 		var enemy = enemy_table.pick_item()
 		var enemy_instance = enemy.scene.instantiate() as Node2D
+		enemy_instance.global_position = get_spawn_position()
+		
+		# TODO: Add mechanism for each mob to set stats based on difficulty
+		enemy_instance.set_difficulty(cur_wave)
 		
 		entities.add_child(enemy_instance)
-		enemy_instance.global_position = get_spawn_position()
-		# TODO: Add mechanism for each mob to set stats based on difficulty
 		
-		total_cost -= enemy.cost
+		
+		target_cost -= enemy.cost
+		actual_cost += enemy.cost
 	
+	$Timer.wait_time = actual_cost + randf_range(-2.0, 3.0)
 	$Timer.start()
