c35b2fea76e5c2579d4e7635526b3c602854358f
Lim Zi Jia <zijialim@gmail.com>
2023-07-16 06:23:47 +0800

    import some sounds

--- scenes/manager/audio_manager.gd
+++ scenes/manager/audio_manager.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-var audio_dictionary: Dictionary
+@onready var audio_dictionary: Dictionary
+@onready var heart_beat: AudioStreamPlayer2D = $heart_beat
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	GameEvents.sound_made.connect(play_audio)
+	
 
 func _init():
 	# Accessing audio directory and making a dictionary
@@ -12,7 +14,7 @@ func _init():
 	audio_directory.list_dir_begin()
 	var audio: String = audio_directory.get_next()
 	while audio != "":
-		if audio.ends_with(".mp3"):
+		if audio.ends_with(".mp3") or audio.ends_with(".wav"):
 			var path_name: String = "res://assets/audio/" + audio
 			var audio_stream: AudioStream = load(path_name)
 			audio_dictionary[audio.get_slice(".", 0)] = audio_stream
@@ -37,3 +39,10 @@ func play_audio(audio: String, volume_db: float, pitch_scale: float) -> void:
 
 func remove_audio(child: AudioStreamPlayer2D) -> void:
 	remove_child(child)
+
+func change_background(audio: String, volume_db: float, pitch_scale: float) -> void:
+	var audio_stream_player: AudioStreamPlayer2D = AudioStreamPlayer2D.new()
+	var audio_stream: AudioStream = audio_dictionary[audio]
+	audio_stream_player.stream = audio_stream
+	audio_stream_player.volume_db = volume_db
+	audio_stream_player.pitch_scale = pitch_scale
---
+++ scenes/manager/heart_beat.gd
@@ -0,0 +1,19 @@
+extends AudioStreamPlayer2D
+var loudest_db: float = 9.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	GameEvents.health_healed.connect(on_health_changed)
+	GameEvents.health_damaged.connect(on_health_changed)
+
+
+func on_health_changed(current_health: float, max_health: float) -> void:
+	play_changed_tween(current_health / max_health)
+
+
+func play_changed_tween(frac_cur_health: float) -> void:
+	var inv_ratio = 1 - frac_cur_health
+	var cur_db = loudest_db * inv_ratio if inv_ratio > 0.5 else -80.0
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "volume_db", cur_db, 0.15)
+
