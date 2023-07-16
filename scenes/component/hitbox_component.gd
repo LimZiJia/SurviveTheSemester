7ecd02d6607ea5fb6c1bfd64a12ff7a44cc41d66
samuelim01 <samuelim01@gmail.com>
2023-07-16 14:25:52 +0800

    Hitbox/hurtbox components can be disabled

--- scenes/component/hitbox_component.gd
+++ scenes/component/hitbox_component.gd
@@ -5,3 +5,9 @@ signal hit
 
 @export var damage := 0.0
 @export var knockback_force := 0.0
+
+@export var disabled := false:
+	set(val):
+		for child in get_children():
+			if child is CollisionShape2D or CollisionPolygon2D:
+				child.disabled = val
