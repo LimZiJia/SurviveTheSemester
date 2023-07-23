5ffe3e23993bb65956e73a26702f9057675edc2e
samuelim01 <samuelim01@gmail.com>
2023-07-23 18:53:11 +0800

    Implemented the papers attack
    1) Fixed the pencil looking at the paper
    2) Added the exam paper spawner node to create "throw spawning" animation
    3) Implemented the paper attacking

--- scenes/game_object/printer/printer.gd
+++ scenes/game_object/printer/printer.gd
@@ -130,7 +130,7 @@ func is_player_obstructed() -> bool:
 		return true
 	
 	var space_state = get_world_2d().direct_space_state
-	# Ray only intersects with world or world objects
+	# Ray only intersects with walls
 	var query = PhysicsRayQueryParameters2D.create(global_position, 
 	player.global_position, 0b1, [self])
 	var result = space_state.intersect_ray(query)
